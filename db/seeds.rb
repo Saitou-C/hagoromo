37ae0227e33c6495e1179522780bb21d50296795
Saitou-C <139321510+Saitou-C@users.noreply.github.com>
2023-11-14 17:42:37 +0000

    Add admin.create! to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: "test@test", ## 任意のメールアドレス,
+  password: "hagoromo_admin" ## 任意のパスワード
+)
