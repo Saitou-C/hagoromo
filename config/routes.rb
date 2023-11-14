823b7d53593cd87ed19e7e9f8bd29f8d6ddf91b3
Saitou-C <139321510+Saitou-C@users.noreply.github.com>
2023-11-14 16:32:58 +0000

    Add devise:views admin&user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  # 管理者用
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 顧客用
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
