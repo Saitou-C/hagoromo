dffdbbd7aaa42a70bc397d0c869469665415a947
Saitou-C <139321510+Saitou-C@users.noreply.github.com>
2023-11-14 18:35:17 +0000

    Add admin/top.html.erb & user/top.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  namespace :admin do#namespaceでurlにadminをつける
+    get "/" => "homes#top"
+  end
+
   # 顧客用
   devise_for :users, controllers: {
     registrations: "user/registrations",
     sessions: 'user/sessions'
   }
+
+  scope module: :user do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
